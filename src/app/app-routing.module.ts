1c0d9c18db08645364435d22f155d84a9671cd90
kumar m <cmynam@gmail.com>
2017-12-07 13:07:36 -0700

    login, profile, orders components are created. app-routing-module updated to include newly created modules.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: "", redirectTo: "/orders", pathMatch: "full"},
+  {path: "login", loadChildren: "./login/login.module#LoginModule"},
+  {path: "profile", loadChildren: "./profile/profile.module#ProfileModule"},
+  {path: "orders", loadChildren: "./orders/orders.module#OrdersModule"}
 ];
 
 @NgModule({
