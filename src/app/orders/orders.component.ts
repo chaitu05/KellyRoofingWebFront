eb28853b37259085173f7163876a2d41a75f09e8
kumar m <cmynam@gmail.com>
2017-12-07 17:06:26 -0700

    Table data displayed after retrieving github issues using angular httpclient.

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,5 +1,14 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from "@angular/material";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {merge} from 'rxjs/observable/merge';
+import {of as observableOf} from 'rxjs/observable/of';
+import {catchError} from 'rxjs/operators/catchError';
+import {map} from 'rxjs/operators/map';
+import {startWith} from 'rxjs/operators/startWith';
+import {switchMap} from 'rxjs/operators/switchMap';
+
 
 @Component({
   selector: 'app-orders',
@@ -8,13 +17,18 @@ import {MatPaginator, MatSort, MatTableDataSource} from "@angular/material";
 })
 export class OrdersComponent implements OnInit {
 
-  displayedColumns = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns = ['created', 'state', 'number', 'title'];
+  exampleDatabase: ExampleHttpDao | null;
+  dataSource = new MatTableDataSource();
+
+  resultsLength = 0;
+  isLoadingResults = false;
+  isRateLimitReached = false;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     // Create 100 users
     const users: UserData[] = [];
     for (let i = 1; i <= 100; i++) {
@@ -33,8 +47,36 @@ export class OrdersComponent implements OnInit {
    * be able to query its view for the initialized paginator and sort.
    */
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+
+    this.exampleDatabase = new ExampleHttpDao(this.http);
+
+    // If the user changes the sort order, reset back to the first page.
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          return this.exampleDatabase!.getRepoIssues(
+            this.sort.active, this.sort.direction, this.paginator.pageIndex);
+        }),
+        map(data => {
+          // Flip flag to show that loading has finished.
+          this.isLoadingResults = false;
+          this.isRateLimitReached = false;
+          this.resultsLength = data.total_count;
+
+          return data.items;
+        }),
+        catchError(() => {
+          this.isLoadingResults = false;
+          // Catch if the GitHub API has reached its rate limit. Return empty data.
+          this.isRateLimitReached = true;
+          return observableOf([]);
+        })
+      ).subscribe(data => this.dataSource.data = data);
+
   }
 
   applyFilter(filterValue: string) {
@@ -72,4 +114,30 @@ export interface UserData {
   name: string;
   progress: string;
   color: string;
+}
+
+export interface GithubApi {
+  items: GithubIssue[];
+  total_count: number;
+}
+
+export interface GithubIssue {
+  created_at: string;
+  number: string;
+  state: string;
+  title: string;
+}
+
+/** An example database that the data source uses to retrieve data for the table. */
+export class ExampleHttpDao {
+  constructor(private http: HttpClient) {
+  }
+
+  getRepoIssues(sort: string, order: string, page: number): Observable<GithubApi> {
+    const href = 'https://api.github.com/search/issues';
+    const requestUrl =
+      `${href}?q=repo:angular/material2&sort=${sort}&order=${order}&page=${page + 1}`;
+
+    return this.http.get<GithubApi>(requestUrl);
+  }
 }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -3,17 +3,23 @@ import {CommonModule} from '@angular/common';
 
 import {OrdersRoutingModule} from './orders-routing.module';
 import {OrdersComponent} from './orders.component';
-import {MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSortModule, MatTableModule} from "@angular/material";
+import {
+  MatFormFieldModule, MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule,
+  MatTableModule
+} from "@angular/material";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
     OrdersRoutingModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatProgressSpinnerModule
   ],
   declarations: [OrdersComponent]
 })
